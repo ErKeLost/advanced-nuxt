9bf45b193895c2633426ffad7495f4710cd32f97
ERKELSOT <1256029807@qq.com>
2023-01-28 08:59:21 +0800

    chore: runtime config env config

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup>
+// 运行在服务端
+if (process.server) {
+  console.log("运行在server");
+}
+if (process.client) {
+  console.log("运行在客户端");
+}
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  // 运行时配置
+  runtimeConfig: {
+    appKey: "loveYou",
+    public: {
+      baseUrl: "https://",
+    },
+  },
+});
